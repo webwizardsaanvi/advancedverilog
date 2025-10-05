bbb9fadb9d124cdef269b1b7b6002d034c200917
Saanvi Suresh <35315204+webwizardsaanvi@users.noreply.github.com>
2025-10-05 15:32:53 -0400

    Update project.v
    yay my work's in here now :)

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_saanvi_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,25 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // Internal resister: 8-bit counter
+    reg [7:0] counter;
+
+    // Sequential logic to increment the counter
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            counter <= 8'b0; // Reset counter to 0
+        else
+            counter <= counter + 1; // Increment counter
+    end
+    //assign counter to output
+    assign uo_out = counter;
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in, uio_in, 1'b0};
 
 endmodule
